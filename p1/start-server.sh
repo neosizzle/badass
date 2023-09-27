59e2076ef8602db5c5fe4f7bae6db1d937d1964a
jng <junhanngd1115@gmail.com>
2023-09-27 09:54:24 +0800

    feat: added installation of dynapips

--- p1/start-server.sh
+++ p1/start-server.sh
@@ -1,6 +1,6 @@
 # start the host docker container
 # docker run -d --name host_jng-1 debian tail -f
-# docker build -t host:latest /host
+# docker build -t host:latest host
 
 # start the router docker container
 
